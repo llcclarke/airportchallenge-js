2aa6ab8b4fe3d4e12f15d3f67a3de25e318b4464
sitypop <sityemilyshah@gmail.com>
2016-05-11 11:46:59 +0100

    Takeoff function test

--- spec/FeaturesSpec.js
+++ spec/FeaturesSpec.js
@@ -12,4 +12,10 @@ describe('Feature Test:', function(){
     plane.land(airport);
     expect(airport.planes()).toContain(plane);
   });
+
+  it('planes can be instructed to takeoff', function(){
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
+  });
 });
