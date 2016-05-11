ba0578928e0bcd37b9b5394cc4da8b969ae4d411
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-05-11 11:42:40 +0100

    passed tests

--- src/Airport.js
+++ src/Airport.js
@@ -1,5 +1,9 @@
 "use strict";
 
-function Airport(){}
-Airport.prototype.planes = function(){ return []; };
-Airport.prototype.clearForLanding = function(plane){};
+function Airport(){
+  this._hanger = [];
+}
+Airport.prototype.planes = function(){ return this._hanger; };
+Airport.prototype.clearForLanding = function(plane){
+  this._hanger.push(plane);
+};
