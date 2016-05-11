1ad843731e047ca2366e71cf5501bdfb3db36afc
sitypop <sityemilyshah@gmail.com>
2016-05-11 11:24:33 +0100

    Airport clearForLanding function

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,11 +1,19 @@
-'use strict';
+(function () {
+   'use strict';
+}());
 
 describe('Airport', function(){
   var airport;
+  var plane;
   beforeEach(function){
     airport = new Airport();
+    plane.jasmine.createSpy('plane');
   });
-it('can recieve a plane', function(){
-  expect(airport.clearForLanding).not.toBeUndefined()
+  it ('has no planes by default', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+  it ('can clear planes for landing', function()){
+    airport.clearForLanding(plane);
+    expect(airport.planes()).toEqual([plane]);
+  };
 });
-  });
--- src/Airport.js
+++ src/Airport.js
@@ -2,3 +2,4 @@
 
 function Airport(){}
 Airport.prototype.planes = function(){ return []; };
+Airport.prototype.clearForLanding = function(plane){};
